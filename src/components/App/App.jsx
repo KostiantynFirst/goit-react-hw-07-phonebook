fa2ac3afc6d802cbad4398bc715e72ab753f0111
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-20 22:36:42 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,13 +5,16 @@ import { ContactList } from "../Contacts/Contacts";
 import { PhonebookContainer, PhonebookHeadings, PhonebookContacts, PhonebookContactsHeading } from "./App.styled";
 import { Filter } from "../Filter/Filter";
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/selectors';
 
 export const App = () => {
 
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError)
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -21,6 +24,7 @@ export const App = () => {
  
     <PhonebookContainer>
       <PhonebookHeadings>Phonebook</PhonebookHeadings>
+      {isLoading && !error && <b>Request in progress...</b>}
     
   <AddContactForm />
   <PhonebookContacts>
